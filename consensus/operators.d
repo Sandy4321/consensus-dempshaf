2fc36560c8ff536965edd3e101519d73ef676f30
TooHuman <michael@toohuman.io>
2018-04-17 16:10:53 +0100

    Removing the left-over operator.

--- consensus/operators.d
+++ consensus/operators.d
@@ -6,33 +6,6 @@ module dempshaf.consensus.operators;
 public final class Operators
 {
     /**
-     * Consensus operator for uncertain three-valued beliefs.
-     */
-    static auto ref beliefConsensus(
-        ref in double[2][] beliefs1,
-        ref in double[2][] beliefs2) pure
-    {
-        auto beliefs = new double[2][beliefs1.length];
-        auto w1 = beliefs1;
-        auto w2 = beliefs2;
-
-        foreach (i, ref belief; beliefs)
-        {
-            belief = [
-                (w1[i][0] * w2[i][1]) +
-                (w1[i][1] * w2[i][0]) -
-                (w1[i][0] * w2[i][0]),
-                (w1[i][0] + w2[i][0]) +
-                (w1[i][1] * w2[i][1]) -
-                (w1[i][1] * w2[i][0]) -
-                (w1[i][0] * w2[i][1])
-            ];
-        }
-
-        return beliefs;
-    }
-
-    /**
      * Consensus operator for Dempster-Shafer mass functions.
      */
 
