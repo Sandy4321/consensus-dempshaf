23725bded9544e256aacc2dae1e89afd544308dd
TooHuman <michael@toohuman.io>
2018-04-18 15:14:29 +0100

    Fixed doc string.

--- misc/importidiom.d
+++ misc/importidiom.d
@@ -1,6 +1,6 @@
 module dempshaf.misc.importidiom;
 
-/*
+/**
  * From is an import idiom that allows you to specify a symbol's
  * module in the arguments list without having to import the symbol
  * in the current module's scope.
