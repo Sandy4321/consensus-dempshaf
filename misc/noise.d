8d4b7deee7471dd397c31ad56b58db0eee8a54a8
TooHuman <michael@toohuman.io>
2018-12-05 13:35:15 +0000

    Added the error function for automatically generating a noise distribution for negative updating.

--- misc/noise.d
+++ misc/noise.d
@@ -14,11 +14,11 @@ auto normalDistribution(ref from!"std.random".Random rand)
     import std.math : cos, log, pow, PI, sin, sqrt;
     import std.random : uniform;
 
-    auto u1 = uniform(0.0, 1.0, rand);
-    auto u2 = uniform(0.0, 1.0, rand);
+    immutable auto u1 = uniform(0.0, 1.0, rand);
+    immutable auto u2 = uniform(0.0, 1.0, rand);
 
-    auto r = sqrt(-2.0 * log(u1));
-    auto theta = 2.0 * PI * u2;
+    immutable auto r = sqrt(-2.0 * log(u1));
+    immutable auto theta = 2.0 * PI * u2;
 
     auto z1 = r * cos(theta);
     auto z2 = r * sin(theta);
@@ -29,11 +29,18 @@ auto normalDistribution(ref from!"std.random".Random rand)
 /**
  *
  */
-auto negComparisonError()
+auto negComparisonError(
+    const double x,
+    const double lambda,
+    ref from!"std.random".Random rand)
 {
-    // Given a number of alternatives, calculate the likelihood of getting
-    // two comparisons wrong, where the comparisons of 1 and 2, is twice as
-    // likely as 1 and 3, etc.
+    import std.math : E;
+    import std.math : pow;
 
-    return 0;
+    // Bound the error function in [0, bound]
+    immutable auto bound = 0.5;
+    auto errorValue = pow(E, -lambda * x) - pow(E, -lambda);
+         errorValue = errorValue / 1 - pow(E, -lambda);
+
+    return bound * errorValue;
 }
