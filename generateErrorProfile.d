ed9c2d5d0feb286dcfc81d57486f499905137456
TooHuman <michael@toohuman.io>
2018-12-05 13:35:41 +0000

    Going to generate figures for the new error function to see which parameters will be appropriate.

---
+++ generateErrorProfile.d
@@ -0,0 +1,85 @@
+module dempshaf.generateErrorProfile;
+
+import dempshaf.misc.normalDistribution;
+
+import std.math, std.stdio, std.string, std.random;
+
+void main(string[] args)
+{
+    immutable auto iterations = 500_000;
+
+    auto rand = Random(unpredictableSeed);
+
+    immutable auto variance = 0.3; // Sigma^2, StdDev = Sigma, sqrt(Sigma^2)
+
+    immutable auto qualitySet = [
+        [0.3, 0.9],
+        [0.5, 1.0],
+        [0.9, 1.0],
+
+        [0.2, 0.4, 1.0],
+        [0.25, 0.5, 0.75],
+        [0.8, 0.9, 1.0]
+    ];
+
+    immutable auto qualities = qualitySet[4];
+    immutable auto choices = qualities.length;
+
+    auto values = new double[][](choices, iterations);
+
+    auto getNoise = true;
+    real[2] noisePair;
+
+    foreach (iteration; 0 .. iterations)
+    {
+        foreach (choice; 0 .. choices)
+        {
+            double noise;
+            double quality = qualities[choice];
+            do
+            {
+                if (getNoise)
+                {
+                    noisePair = normalDistribution(rand);
+                    noise = noisePair[0];
+                    getNoise = false;
+                }
+                else
+                {
+                    noise = noisePair[1];
+                    getNoise = true;
+                }
+
+                noise *= variance;
+
+            } while (quality + noise < 0 || quality + noise > 1);
+            quality += noise;
+
+            if (quality > 1)      values[choice][iteration] = 1.0;
+            else if (quality < 0) values[choice][iteration] = 0.0;
+            else                  values[choice][iteration] = quality;
+        }
+    }
+
+    // Write the results
+
+    string directory = "../results/test_results/dempshaf/";
+
+    auto file = File("noise_results.csv", "w");
+
+    file.write(format("%.4f\n", variance));
+    foreach (choice; 0 .. choices)
+    {
+        file.write(format("%.2f", qualities[choice]));
+        file.write((choice == choices - 1) ? "\n" : ",");
+    }
+    foreach (choice; 0 .. choices)
+    {
+        foreach (iteration; 0 .. iterations)
+        {
+            file.write(format("%.4f", values[choice][iteration]));
+            file.write((iteration == cast(ulong) values[choice].length - 1) ? "\n" : ",");
+        }
+    }
+    file.close();
+}
